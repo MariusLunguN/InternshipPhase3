9e5e58ffbd06e87422e8e50a74a5ceb8398db3bc
Marius Lungu <marius_lng@yahoo.com>
2021-11-03 16:38:57 +0200

    bundled all scripts + fixed fonts issue

--- Website/App_Start/BundleConfig.cs
+++ Website/App_Start/BundleConfig.cs
@@ -8,19 +8,21 @@ namespace Website
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.9.1.min.js",
+                        "~/Scripts/jquery.ba-cond.min",
+                        "~/Scripts/jquery.slitslider.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.6.2-respond-1.1.0.min.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/otherScripts").Include(
+                        "~/Scripts/main.js",
+                        "~/Scripts/twitter-bootstrap-hover-dropdown.min.js"));
+
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-responsive.min.css",
