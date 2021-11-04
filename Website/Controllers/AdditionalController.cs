ff7096d1af1ac666578cd83739bca21cd080ffa9
sfr <sfr@nemetosro.onmicrosoft.com>
2021-11-04 10:56:04 +0200

    Finished static pages

---
+++ Website/Controllers/AdditionalController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class AdditionalController : Controller
+    {
+        // GET: Additional
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        public ActionResult NotFoundPage()
+        {
+            return View();
+        }
+    }
+}
