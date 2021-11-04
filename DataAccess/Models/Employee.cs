17cbb74b9d06abb88194890e489f8e7c7e2d6bdf
Marius Lungu <marius_lng@yahoo.com>
2021-11-04 11:47:56 +0200

    Created DataAccess class library and installed EF

---
+++ DataAccess/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public object Picture { get; set; }
+
+        [Required]
+        public string Intro { get; set; }
+
+        [Required]
+        public IEnumerable<SocialMedia> SocialMedia { get; set; }
+    }
+}
---
+++ DataAccess/Models/SocialMedia.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess.Models
+{
+    public class SocialMedia
+    {
+        public int ID { get; set; }
+        public string Url { get; set; }
+    }
+}
